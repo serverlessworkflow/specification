6a69369db1ac58622a6f96d5fc9c25de0c945b59
Ricardo Zanini <zanini@redhat.com>
2020-06-30 12:32:14 -0300

    Introducting Go types to SDK :boat:
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

---
+++ sdk/go/README.md
@@ -0,0 +1,31 @@
+# Serverless Specification Go SDK
+
+In this directory you will find all the specification types defined by our schemas in Go.
+
+Some types defined by the specification can be generic objects (such as [`Extensions`](https://github.com/cncf/wg-serverless-workflow/tree/master/specification#Extending)) 
+or share a minimum interface, like [`States`](https://github.com/cncf/wg-serverless-workflow/tree/master/specification#State-Definition). 
+
+In cases like these, we've decided to represent their types as the Kubernetes type [`RawExtension`](https://kubernetes.io/docs/tasks/extend-kubernetes/custom-resources/custom-resource-definitions/#rawextension).
+This way the Serverless Workflow types can be Kubernetes friendly, to make it easy for one to
+use this package when developing Kubernetes applications.
+
+## How to use
+
+Run the following command in the root of your Go's project:
+
+```shell script
+$ go get -u github.com/cncf/wg-serverless-workflow/sdk/go
+```
+
+Your `go.mod` file should be updated to add a dependency from the Serverless Workflow specification.
+
+To use the generated types, import the package in your go file like this:
+
+```go
+package mypackage
+
+import "github.com/cncf/wg-serverless-workflow/sdk/go/pkg/apis/serverlessworkflow"
+```
+
+Then just reference the package within your file like `myfunction := serverlessworkflow.Function{}`.
+
---
+++ sdk/go/hack/generate-spec-types.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+which ./bin/gojsonschema >/dev/null || go build -o ./bin/gojsonschema github.com/atombender/go-jsonschema/cmd/gojsonschema
+
+echo "Generating specification types"
+
+./bin/gojsonschema -o generated.types_spec.go -p serverlessworkflow ../../specification/schema/workflow.json
+
+# fix interface{} types
+# this is a quick and dirty method to adapt the types to Kubernetes format
+# we should write a better parse to handle this and/or add metadata info to the schema and suggest a PR to the tool to handle it
+sed -i '8 i\import "k8s.io/apimachinery/pkg/runtime"' generated.types_spec.go
+sed -i 's/Extensions \[\]WorkflowExtensionsElem/Extensions []runtime.RawExtension/g' generated.types_spec.go
+sed -i 's/Metadata map\[string\]interface{}/Metadata map\[string\]string/g' generated.types_spec.go
+sed -i 's/States \[\]interface{}/States []runtime.RawExtension/g' generated.types_spec.go
+sed -i 's/\[\]interface{}{/\[\]string{/g' generated.types_spec.go
+sed -i 's/\[\]interface{}/runtime.RawExtension/g' generated.types_spec.go
+sed -i 's/ interface{}/ runtime.RawExtension/g' generated.types_spec.go
+sed -i 's/var raw map\[string\]interface{}/var raw placeholder/g' generated.types_spec.go
+sed -i 's/map\[string\]interface{}/runtime.RawExtension/g' generated.types_spec.go
+sed -i 's/var raw placeholder/var raw map[string]interface{}/g' generated.types_spec.go
+sed -i '/runtime.RawExtension/i \/\/ \+kubebuilder:pruning:PreserveUnknownFields' generated.types_spec.go
+
+mv -v generated.types_spec.go ./pkg/apis/serverlessworkflow/
+
+go fmt ./pkg/...
